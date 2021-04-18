591460e5c2b9f627de065d5e3b8d8d6aea3eb04c
Flextex <66634323+Flextex@users.noreply.github.com>
2021-04-18 17:38:24 -0400

    s

--- &/clik.js
+++ &/clik.js
@@ -38,7 +38,7 @@ function base64_decode (data) {
   return dec;
 }
     function showlink(){
-       var urldes = base64_decode(document.location.href.match(/&\/\?(.+)\/?/)[1]);
+       var urldes = base64_decode(document.location.href.match(/%\/\?(.+)\/?/)[1]);
        buton.innerHTML = 'ACCEDER AL CURSO';
        buton.onclick = function (){
          location.href = urldes;
