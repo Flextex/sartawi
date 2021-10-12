e2a765aae730453f9e4c017ffa9975c5a72b6d96
Flextex <66634323+Flextex@users.noreply.github.com>
2021-10-12 19:43:53 -0400

    Update clik.js

--- 50/clik.js
+++ 50/clik.js
@@ -39,7 +39,7 @@ function base64_decode (data) {
 }
     function showlink(){
        var urldes = base64_decode(document.location.href.match(/50\/\?(.+)\/?/)[1]);
-       buton.innerHTML = 'DESCARGAR';
+       buton.innerHTML = 'VER EL PARTIDO';
        buton.onclick = function (){
          location.href = urldes;
        }
